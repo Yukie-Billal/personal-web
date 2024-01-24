39fee8d7c0c11dc1099db0ac730c93e539908b41
Yukie M Billal <yukiembillal01@gmail.com>
2024-01-24 18:23:58 +0700

    feat: SEO for portofolio & root route

--- web-page/pages/index.vue
+++ web-page/pages/index.vue
@@ -1,4 +1,13 @@
 <script>
+useSeoMeta({
+   title: 'My Amazing poersonal website',
+   ogTitle: 'Yukie muhammad billal personal website',
+   description: 'My personal website, documentations, project, story, blog. all in one',
+   ogDescription: 'Yukie personal web, feel free to explore. Learn something by doing, My story life in 1 place.',
+   ogImage: 'https://yukie.site/yukie.jpg',
+   ogImageAlt: 'Yukie muhammad billal',
+   twitterCard: 'summary_large_image'
+})
 try {
    window.location = '/portofolio'
 } catch (e) {}
--- web-page/pages/portofolio.vue
+++ web-page/pages/portofolio.vue
@@ -1,4 +1,14 @@
 <script setup lang="ts">
+useSeoMeta({
+   title: 'My Amazing poersonal website',
+   ogTitle: 'Yukie muhammad billal personal website',
+   description: 'My personal website, documentations, project, story, blog. all in one',
+   ogDescription: 'Yukie personal web, feel free to explore. Learn something by doing, My story life in 1 place.',
+   ogImage: 'https://yukie.site/yukie.jpg',
+   ogImageAlt: 'Yukie muhammad billal',
+   twitterCard: 'summary_large_image'
+})
+
 import 'assets/css/main.css'
 import 'assets/css/keyframes.css'
 import 'assets/css/portofolio/portofolio.css'
