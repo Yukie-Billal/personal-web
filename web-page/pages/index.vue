b6d71b9c92685b4d1e862b16fb92e6fdb378f4da
Yukie M Billal <yukiembillal01@gmail.com>
2024-01-24 19:57:49 +0700

    feat: addogImage

--- web-page/pages/index.vue
+++ web-page/pages/index.vue
@@ -4,7 +4,7 @@ useSeoMeta({
    ogTitle: 'Yukie muhammad billal personal website',
    description: 'My personal website, documentations, project, story, blog. all in one',
    ogDescription: 'Yukie personal web, feel free to explore. Learn something by doing, My story life in 1 place.',
-   ogImage: 'https://yukie.site/yukie.jpg',
+   ogImage: 'https://yukie.site/main-background.png',
    ogImageAlt: 'Yukie muhammad billal',
    twitterCard: 'summary_large_image'
 })
