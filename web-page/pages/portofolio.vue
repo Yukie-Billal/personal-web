cfe2444a9c02268ff295deb1112ca2679d3fc0d8
Yukie M Billal <yukiembillal01@gmail.com>
2024-01-24 21:06:12 +0700

    ui: responsive < 1280px

--- web-page/pages/portofolio.vue
+++ web-page/pages/portofolio.vue
@@ -13,6 +13,7 @@ import 'assets/css/main.css'
 import 'assets/css/keyframes.css'
 import 'assets/css/portofolio/portofolio.css'
 import 'assets/css/portofolio/portofolio-1024.css'
+import 'assets/css/portofolio/portofolio-1280.css'
 import Home from "~/components/portofolio/Home.vue"
 
 const selectedPage: string= "Home"
