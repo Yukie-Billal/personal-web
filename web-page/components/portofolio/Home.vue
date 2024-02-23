7aeaaa0a9fa4696f8eb34d947f366c0317b35443
Yukie M Billal <yukiembillal01@gmail.com>
2024-02-23 13:41:04 +0700

    feat: pnpm, navigation icon

--- web-page/components/portofolio/Home.vue
+++ web-page/components/portofolio/Home.vue
@@ -3,14 +3,5 @@
 </script>
 
 <template>
-   <div class="profile-wrapper">
-      <div class="profile-box">
-         <p>Hi, I'am</p>
-         <p>Yukie Muhammad Billal</p>
-         <p>Junior fullstack web development</p>
-      </div>
-   </div>
-   <div class="image-profile-wrapper">
-      <img src="/tet-chibi.png" alt="Ini gambar" width="437" />
-   </div>
+
 </template>
--- web-page/pages/portofolio.vue
+++ web-page/pages/portofolio.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 useSeoMeta({
-   title: 'My Amazing poersonal website',
+   title: 'Yukie muhammad billal personal website',
    ogTitle: 'Yukie muhammad billal personal website',
    description: 'My personal website, documentations, project, story, blog. all in one',
    ogDescription: 'Yukie personal web, feel free to explore. Learn something by doing, My story life in 1 place.',
@@ -21,6 +21,25 @@ const selectedPage: string= "Home"
 
 <template>
    <div class="content-wrapper">
-      <Home v-if="selectedPage == 'Home'" />
+      <div class="navigation-wrapper">
+         <ul class="wrapper">
+            <li class="navigation-item">
+               <Icon name="bxs:home" />
+            </li>
+            <li class="navigation-item">
+               <Icon name="bxs:user" />
+            </li>
+         </ul>
+      </div>
+      <div class="profile-wrapper" v-if="selectedPage == 'Home'">
+         <div class="profile-box">
+            <p>Hi, I'am</p>
+            <p>Yukie Muhammad Billal</p>
+            <p>Junior fullstack web development</p>
+         </div>
+      </div>
+      <div class="image-profile-wrapper">
+         <img src="/tet-chibi.png" alt="Ini gambar" width="437" />
+      </div>
    </div>
 </template>
